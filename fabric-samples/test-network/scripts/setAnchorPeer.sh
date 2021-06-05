67375677b4e99c502c6b36a5cc6d379e032f97fe
grapebaba <281165273@qq.com>
2021-06-05 17:38:15 +0800

    Change to no tls

--- fabric-samples/test-network/scripts/setAnchorPeer.sh
+++ fabric-samples/test-network/scripts/setAnchorPeer.sh
@@ -42,7 +42,8 @@ createAnchorPeerUpdate() {
 }
 
 updateAnchorPeer() {
-  peer channel update -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ${CORE_PEER_LOCALMSPID}anchors.tx --tls --cafile $ORDERER_CA >&log.txt
+  #peer channel update -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ${CORE_PEER_LOCALMSPID}anchors.tx --tls --cafile $ORDERER_CA >&log.txt
+  peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ${CORE_PEER_LOCALMSPID}anchors.tx >&log.txt
   res=$?
   cat log.txt
   verifyResult $res "Anchor peer update failed"
